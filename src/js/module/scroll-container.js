b64745e59bc59329cdb0ca9e2fd56455b348d7ab
Niv Suresh <niv.ssuresh@ft.com>
2018-12-03 16:38:52 +0000

    Final rollup

--- src/js/module/scroll-container.js
+++ src/js/module/scroll-container.js
@@ -1,12 +1,29 @@
 import scrollMonitor from 'scrollmonitor'
 import _ from 'lodash'
+import {isDesktop} from 'util/breakpoints'
 
 let textOne = document.querySelector('.scroll-window.-one .scroll-window-content')
 
 window.onbeforeunload = function () {
-  window.scrollTo(0, 0)
-  textOne.scrollIntoView({behavior: 'instant', block: 'center', inline: 'nearest'})
+/*  window.scrollTo(0, 0)
+  textOne.scrollIntoView({behavior: 'smooth', block: 'center', inline: 'nearest'})*/
 }
+
+let scrollPos = 0
+let isScrollDown = false
+const docBody = document.querySelector('.document-body')
+console.log(docBody)
+
+window.addEventListener('scroll', _.throttle((e) => {
+  // detects new state and compares it with the new one
+  if ((document.body.getBoundingClientRect()).top > scrollPos) {
+    isScrollDown = false
+  } else {
+    isScrollDown = true
+  }
+  // saves the new position for iteration.
+  scrollPos = (document.body.getBoundingClientRect()).top
+}, 500))
 
 const pageContainer = document.querySelector('.m-page-container')
 const textOneWatcher = scrollMonitor.create(textOne)
@@ -32,12 +49,11 @@ const textSevenWatcher = scrollMonitor.create(textSeven)
 const textEight = document.querySelector('.scroll-window.-eight .scroll-window-content')
 const textEightWatcher = scrollMonitor.create(textEight)
 
-const img1 = document.querySelector('.m-background-img-01')
-const img2 = document.querySelector('.m-background-img-02')
-const img3 = document.querySelector('.m-background-img-03')
-const img4 = document.querySelector('.m-background-img-04')
-const img5 = document.querySelector('.m-background-img-05')
-const img6 = document.querySelector('.m-background-img-06')
+const textNine = document.querySelector('.scroll-window.-nine .scroll-window-content')
+const textNineWatcher = scrollMonitor.create(textNine)
+
+const textTen = document.querySelector('.scroll-window.-ten .scroll-window-content')
+const textTenWatcher = scrollMonitor.create(textTen)
 
 /* Banner scroll button  */
 const heroBannerScroll = document.querySelector('.scroll-window__btn')
@@ -46,147 +62,141 @@ heroBannerScroll.addEventListener('click', (e) => {
   textTwo.scrollIntoView({behavior: 'smooth', block: 'center', inline: 'nearest'})
 })
 
-let scrollPos = 0
-let isScrollDown = false
-
-window.addEventListener('scroll', _.throttle((e) => {
-  // detects new state and compares it with the new one
-  if ((document.body.getBoundingClientRect()).top > scrollPos) {
-    console.log('up')
-    isScrollDown = false
-  } else {
-    console.log('down')
-    isScrollDown = true
-  }
-  // saves the new position for iteration.
-  scrollPos = (document.body.getBoundingClientRect()).top
-}, 500))
-
 /* TextOne */
 textOneWatcher.enterViewport(function () {
-  console.log('I have entered the viewport')
+  pageContainer.classList.remove('-two', '-three', '-four', '-five', '-six', '-seven', '-eight', '-nine', '-darken')
+  pageContainer.classList.add('-one')
 })
 
 textOneWatcher.exitViewport(function () {
-  if (isScrollDown) {
-    pageContainer.classList.add('-darken')
-  }
 })
 
 textOneWatcher.fullyEnterViewport(function () {
-  if (!isScrollDown) {
-    pageContainer.classList.remove('-darken')
-  }
-  img1.classList.add('-show')
+  pageContainer.classList.remove('-two', '-three', '-four', '-five', '-six', '-seven', '-eight', '-nine')
+  pageContainer.classList.add('-one')
 })
 
 /* TextTwo */
 textTwoWatcher.enterViewport(function () {
-  console.log('I have entered the viewport2')
+  pageContainer.classList.add('-darken', '-one')
+  pageContainer.classList.remove('-two', '-three', '-four', '-five', '-six', '-seven', '-eight', '-nine')
 })
-textTwoWatcher.exitViewport(function () {
-  if (isScrollDown) {
-    img1.classList.remove('-show')
-    img2.classList.add('-show')
-  }
 
-  pageContainer.classList.remove('-darken')
+textTwoWatcher.exitViewport(function () {
+  if (textThreeWatcher.isInViewport) {
+    pageContainer.classList.add('-two')
+    pageContainer.classList.remove('-one', '-darken')
+  }
 })
 
 textTwoWatcher.fullyEnterViewport(function () {
-  if (isScrollDown) {
-    pageContainer.classList.add('-darken')
-  } else {
-    pageContainer.classList.remove('-darken')
-  }
 })
 
-/* TextThree */
 textThreeWatcher.enterViewport(function () {
-  /*  pageContainer.classList.remove('-darken', '-white')*/
+  pageContainer.classList.add('-two')
+  pageContainer.classList.remove('-one', '-three', '-four', '-five', '-six', '-seven', '-eight', '-nine')
 })
+
 textThreeWatcher.exitViewport(function () {
-  if (isScrollDown) {
-    img2.classList.remove('-show')
-    img3.classList.add('-show')
-  }
+
 })
 
 textThreeWatcher.fullyEnterViewport(function () {
-  img2.classList.add('-show')
-  img1.classList.remove('-show')
+
 })
 
-/* TextFour */
 textFourWatcher.enterViewport(function () {
-  console.log('I have entered the viewport2')
+  pageContainer.classList.add('-three')
+  pageContainer.classList.remove('-one', '-two', '-four', '-five', '-six', '-seven', '-eight', '-nine')
 })
 textFourWatcher.exitViewport(function () {
-  if (isScrollDown) {
-    img4.classList.add('-show')
-    img3.classList.remove('-show')
-  }
+
 })
 
 textFourWatcher.fullyEnterViewport(function () {
-  /*  pageContainer.classList.add('-darken')
-    pageContainer.classList.remove('-white')*/
 })
 
-/* TextFive */
 textFiveWatcher.enterViewport(function () {
-  console.log('I have entered the viewport2')
+  pageContainer.classList.add('-four')
+  pageContainer.classList.remove('-one', '-two', '-three', '-five', '-six', '-seven', '-eight', '-nine', '-darken')
 })
+
 textFiveWatcher.exitViewport(function () {
-  if (isScrollDown) {
-    img2.classList.add('-show')
-    img4.classList.remove('-show')
-  }
 })
 
 textFiveWatcher.fullyEnterViewport(function () {
-  /*  pageContainer.classList.add('-white')
-    pageContainer.classList.remove('-darken')*/
 })
 
-/* TextSix */
 textSixWatcher.enterViewport(function () {
+  pageContainer.classList.add('-five')
+  pageContainer.classList.remove('-one', '-two', '-three', '-four', '-six', '-seven', '-eight', '-nine', '-darken')
 })
 textSixWatcher.exitViewport(function () {
-  if (isScrollDown) {
-    img2.classList.remove('-show')
-    img5.classList.add('-show')
-  }
-})
-textSixWatcher.fullyEnterViewport(function () {
-  /*  pageContainer.classList.remove('-white')
-    pageContainer.classList.add('-thirdImg')*/
+
 })
 
-/* TextSeven */
+textSixWatcher.fullyEnterViewport(function () {
+})
+
 textSevenWatcher.enterViewport(function () {
+  pageContainer.classList.add('-six')
+  pageContainer.classList.remove('-one', '-two', '-three', '-four', '-five', '-seven', '-eight', '-nine', '-darken')
 })
 textSevenWatcher.exitViewport(function () {
 
 })
+
 textSevenWatcher.fullyEnterViewport(function () {
-  /*
-    pageContainer.classList.remove('-final', '-darken')
-  */
 })
 
-/* TextEight */
 textEightWatcher.enterViewport(function () {
-
+  pageContainer.classList.add('-seven')
+  pageContainer.classList.remove('-one', '-two', '-three', '-four', '-five', '-six', '-eight', '-nine', '-darken')
 })
 
 textEightWatcher.exitViewport(function () {
-  /*  if (!isScrollDown) {
-      pageContainer.classList.remove('-final', '-darken')
-    }*/
+
 })
+
 textEightWatcher.fullyEnterViewport(function () {
-  /*
-    pageContainer.classList.add('-final', '-darken')
-  */
+
+})
+
+textNineWatcher.enterViewport(function () {
+  pageContainer.classList.add('-eight', '-darken')
+  pageContainer.classList.remove('-one', '-two', '-three', '-four', '-five', '-six', '-seven', '-nine')
+})
+
+textNineWatcher.exitViewport(function () {
+
+})
+textNineWatcher.fullyEnterViewport(function () {
+
+})
+textTenWatcher.enterViewport(function () {
+  pageContainer.classList.add('-nine')
+  pageContainer.classList.remove('-one', '-two', '-three', '-four', '-five', '-six', '-seven', '-eight', '-darken')
+})
+
+textTenWatcher.exitViewport(function () {
+
+})
+textTenWatcher.fullyEnterViewport(function () {
+
+})
+
+const toolTipContainer = document.querySelector('.tooltip-info')
+const toolTipImg = toolTipContainer.querySelector('img')
+
+console.log(toolTipImg)
+
+toolTipContainer.addEventListener('click', (e) => {
+  console.log('sdnjas')
+  toolTipContainer.classList.toggle('-hover')
+})
+
+const scrollTop = document.querySelector('.footer-navigate-top')
+scrollTop.addEventListener('click', (e) => {
+  e.preventDefault()
+  textOne.scrollIntoView({behavior: 'smooth', block: 'center', inline: 'nearest'})
 })
